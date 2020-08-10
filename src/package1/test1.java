7fbf5e10bb7721b511fa93fd17e70f38f7c2cf9a
abhandary11 <abhishekbhandary@outlook.com>
2020-08-10 21:06:03 +0530

    this is my first commit

---
+++ src/package1/test1.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Start");
+		
+		String currentDirectory = System.getProperty("user.dir");
+	    System.out.println("user.dir: " + currentDirectory);
+	    
+	    System.out.println("Test Passed");
+
+	}
+
+}
